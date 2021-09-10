69beeef0a801eabfdbc713d147d55d62787ff9ec
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-10 14:19:22 -0300

    Extracting info

--- js/app.js
+++ js/app.js
@@ -17,7 +17,21 @@ function agregarCurso(e) {
     e.preventDefault();
 
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target.parentNode);
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        leerDatosCurso(cursoSeleccionado);
     }
 
+}
+
+function leerDatosCurso(curso) {
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1,
+    }
+    console.log(infoCurso);
 }
